161836a587195a96faa0840d9689c57d855f0b4e
AnilaWaqar <160339061+AnilaWaqar@users.noreply.github.com>
2024-03-18 19:32:18 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var userNames = ['admin', 'Waqar', 'Anila', 'Rohaan', 'Jabbar'];
-for (var i = 0; i < userNames.length; i++) {
-    if (userNames[i] === 'admin') {
-        console.log('Hello admin, would you like to see the status report?');
-    }
-    else {
-        console.log("Hello ".concat(userNames[i], ", you have successfully logged in again!"));
-    }
+// List of animals with a common characteristic
+var animals = ["dog", "goat", "cow"];
+// Use a for loop to print the name of each animal
+console.log("Names of animals:");
+for (var i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
 }
+//Use a for loop to print a statement about each animal
+console.log("\nStatements about each animal:");
+for (var i = 0; i < animals.length; i++) {
+    console.log("A ".concat(animals[i], " would make a great pet."));
+}
+//  common characteristic
+console.log("\nAny of these animals would make a great pet!");
+//  common characteristic
+console.log("They are all mammals.");
--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
+// List of animals with a common characteristic
+const animals = ["dog", "goat", "cow"];
 
-
-let userNames: string[] = ['admin', 'Waqar', 'Anila', 'Rohaan', 'Jabbar'];
-for (let i = 0; i < userNames.length; i++) {
-    if (userNames[i] === 'admin') {
-        console.log('Hello admin, would you like to see the status report?');
-    } else {
-        console.log(`Hello ${userNames[i]}, you have successfully logged in again!`);
-    }
+// Use a for loop to print the name of each animal
+console.log("Names of animals:");
+for (let i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
 }
+//Use a for loop to print a statement about each animal
+console.log("\nStatements about each animal:");
+for (let i = 0; i < animals.length; i++) {
+    console.log(`A ${animals[i]} would make a great pet.`);
+}
+
+//  common characteristic
+console.log("\nAny of these animals would make a great pet!");
+
+//  common characteristic
+console.log("They are all mammals.");
