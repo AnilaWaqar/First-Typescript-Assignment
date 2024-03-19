3cfc5ac293728e5e4ce97e9961b39e3bba6ee63c
AnilaWaqar <160339061+AnilaWaqar@users.noreply.github.com>
2024-03-18 19:08:20 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-"use strict";
-// LowerCase
-Object.defineProperty(exports, "__esModule", { value: true });
-var personName = 'Anu Waqar';
-console.log('lowerCase:', personName.toLowerCase());
-// UpperCase
-var personName1 = "Anu Waqar";
-console.log('UpperCase:', personName1.toLocaleUpperCase());
-// Tittle Case
-var personName2 = "Anu Waqar";
-console.log('titleCase:', personName2.replace(/\bw/g, function (c) { return c.toUpperCase(); })); //
-// simple way to write all sensitive case.
-var personName3 = "Anu Waqar";
-console.log('anu waqar\nANU WAQAR\nAnu Waqar');
+console.log("Steve Job Once Said", '"The only way to do great work is to love what you do"');
--- app.ts
+++ app.ts
@@ -1,21 +1,4 @@
-// LowerCase
+ console.log("Steve Job Once Said", '"The only way to do great work is to love what you do"');
 
-import { title } from "process";
-
-let personName: string = 'Anu Waqar'
-console.log('lowerCase:', personName.toLowerCase () ); 
-
-// UpperCase
-
-let personName1: string = "Anu Waqar"
-console.log('UpperCase:',personName1.toLocaleUpperCase () );
-
-// Tittle Case
-let personName2: string = "Anu Waqar"
-console.log('titleCase:', personName2.replace(/\bw/g,c => c.toUpperCase())); //
-
-// simple way to write all sensitive case.
-let personName3: string = "Anu Waqar"
-console.log('anu waqar\nANU WAQAR\nAnu Waqar');
 
 
