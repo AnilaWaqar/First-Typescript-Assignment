489b05c5e4389cc581f5188920b57702feb64aea
AnilaWaqar <160339061+AnilaWaqar@users.noreply.github.com>
2024-03-18 19:35:42 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-// List of animals with a common characteristic
-var animals = ["dog", "goat", "cow"];
-// Use a for loop to print the name of each animal
-console.log("Names of animals:");
-for (var i = 0; i < animals.length; i++) {
-    console.log(animals[i]);
+function makeAlbum(artist, title, tracks) {
+    var album = {
+        artist: artist,
+        title: title
+    };
+    // If tracks parameter is provided, add it to the album object
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+    return album;
 }
-//Use a for loop to print a statement about each animal
-console.log("\nStatements about each animal:");
-for (var i = 0; i < animals.length; i++) {
-    console.log("A ".concat(animals[i], " would make a great pet."));
-}
-//  common characteristic
-console.log("\nAny of these animals would make a great pet!");
-//  common characteristic
-console.log("They are all mammals.");
+// Make three dictionaries representing different albums and print each return value
+var album1 = makeAlbum('Artist1', 'Album1');
+var album2 = makeAlbum('Artist2', 'Album2'); // Including the number of tracks
+var album3 = makeAlbum('Artist3', 'Album3', 12);
+console.log(album1);
+console.log(album2);
+console.log(album3);
--- app.ts
+++ app.ts
@@ -1,19 +1,29 @@
-// List of animals with a common characteristic
-const animals = ["dog", "goat", "cow"];
-
-// Use a for loop to print the name of each animal
-console.log("Names of animals:");
-for (let i = 0; i < animals.length; i++) {
-    console.log(animals[i]);
-}
-//Use a for loop to print a statement about each animal
-console.log("\nStatements about each animal:");
-for (let i = 0; i < animals.length; i++) {
-    console.log(`A ${animals[i]} would make a great pet.`);
+interface Album {
+    artist: string;
+    title: string;
+    tracks?: number; // Optional parameter for the number of tracks
 }
 
-//  common characteristic
-console.log("\nAny of these animals would make a great pet!");
+function makeAlbum(artist: string, title: string, tracks?: number): Album {
+    let album: Album = {
+        artist: artist,
+        title: title
+    };
 
-//  common characteristic
-console.log("They are all mammals.");
+    // If tracks parameter is provided, add it to the album object
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+
+    return album;
+}
+
+// Make three dictionaries representing different albums and print each return value
+let album1: Album = makeAlbum('Artist1', 'Album1');
+let album2: Album = makeAlbum('Artist2', 'Album2');
+let album3: Album = makeAlbum('Artist3', 'Album3', 12); // Including the number of tracks
+
+
+console.log(album1);
+console.log(album2);
+console.log(album3);
