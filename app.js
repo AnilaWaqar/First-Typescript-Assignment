2867f029fa6c391700d601851f9b844ccdc51972
AnilaWaqar <160339061+AnilaWaqar@users.noreply.github.com>
2024-03-18 19:07:10 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+var personName = "Anila"; /*save your name in a variable*/
+var message = "Hello ".concat(personName, ", have you done your assignment?"); /*$ is used to call the variable*/
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,3 @@
+let personName: string = "Anila";/*save your name in a variable*/
+let message: string = `Hello ${personName}, have you done your assignment?`;/*$ is used to call the variable*/
+console.log(message);
